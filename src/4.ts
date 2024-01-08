88cbd0933d80a0e8c6f4c4ccc8891d48c1e26d16
Nina Pecherska <npecherska@gmail.com>
2024-01-08 22:38:01 +0200

    Initial commit

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+  abstract openDoor(key: Key): void;
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("The door is open.");
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is opened.");
+    } else {
+      console.log("Invalid key. The door remains closed.");
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +54,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
